4dee557eedee8f714813901dc89e92feae97eadd
breketmax <maxim_228228228@mail.ru>
2023-01-22 15:04:25 +0700

    Void

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchCustomers } from "./asyncAction/customers";
+import { addCustomerAction, removeCustomerAction } from "./store/customerReducer";
 
-function App() {
+
+const App = ()=> {
+  const dispatch = useDispatch();
+  const cash = useSelector(state => state.cash.cash)
+  const customers = useSelector(state => state.customer.customers);
+
+  const addCash = (cash) => {
+    dispatch({type:"ADD_CASH",payload:cash})
+  };
+
+  
+  const getCash = (cash) => {
+    dispatch({type:"GET_CASH",payload:cash})
+  };
+
+  const addCustomer = (name) =>{
+    const newCustomer ={
+      name,
+      id:Date.now()
+    }
+    dispatch(addCustomerAction( newCustomer))
+  }
+
+  const removeCustomer = (customer) =>{
+    dispatch(removeCustomerAction(customer.id))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>{cash}</div>
+        <button onClick={() => addCash(+prompt())}>Add cash</button>
+        <button onClick={() => getCash(+prompt())}>Get cash</button>
+        <button onClick={() => addCustomer(prompt())}>Add customer</button>
+        <button onClick={() => getCash(+prompt())}>Get cash</button>
+        <button onClick={() => dispatch(fetchCustomers())}>Get clients from db</button>
+        <div>
+          {customers.length > 0 ?
+            <div>
+              {customers.map(cus => 
+                <div onClick={()=>removeCustomer(cus)}>
+                  {cus.name}
+                </div>
+              )}
+            </div>
+            :<h1>
+              лкиенты 0
+            </h1>
+          }
+        </div>
     </div>
   );
 }
